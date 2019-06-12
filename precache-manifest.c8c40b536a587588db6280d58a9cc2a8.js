fda024b656fc9aa1371004d78e8a53f5941e3bb9
AlexHladun <gefest.gl@gmail.com>
2019-06-12 10:53:18 +0300

    Updates

---
+++ precache-manifest.c8c40b536a587588db6280d58a9cc2a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d1e048e754126bc8d9096103b7d706",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "fb2fe7a5030acdbc6259",
+    "url": "/react_dynamic-list-of-posts/static/css/main.ddc338a9.chunk.css"
+  },
+  {
+    "revision": "4e46e9d638db29490995",
+    "url": "/react_dynamic-list-of-posts/static/js/2.20e50229.chunk.js"
+  },
+  {
+    "revision": "fb2fe7a5030acdbc6259",
+    "url": "/react_dynamic-list-of-posts/static/js/main.03867725.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
